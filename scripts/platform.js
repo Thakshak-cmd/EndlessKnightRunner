1c4686684c93e08b6ff8cb047ce560ad3a85e1dd
Thakshak <thakshak120@gmail.com>
2023-03-21 20:23:47 +0800

    first commit

---
+++ scripts/platform.js
@@ -0,0 +1,5 @@
+ function drawPlatform(ctx, canvas) {
+    // Draw the platform
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, canvas.height - 50, canvas.width, 50);
+    }
---
+++ scripts/player.js
@@ -0,0 +1,8 @@
+ function animatePlayer(ctx, player) {
+    // Draw the player
+    ctx.fillStyle = player.fillStyle;
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+    
+    // Update the player's animation
+ 
+    }
