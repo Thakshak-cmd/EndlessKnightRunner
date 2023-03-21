8c93a31b074b107ac4cdd72a356a0e6352f8c2cc
divss12 <divyanshrbhutra@gmail.com>
2023-03-21 22:03:05 +0800

    added skeleton code structure like draw loop and enabled canvas to be resized with window resize

---
+++ scripts/draw.js
@@ -0,0 +1,17 @@
+let player = new Player();
+
+
+let lastTimeStamp = 0;
+let frame = 0;
+
+function draw(timestamp=0){
+    frame = timestamp - lastTimeStamp;
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, 469, 279);    
+
+    //all game code goes here
+    player.draw(frame);
+
+    lastTimeStamp = timestamp;
+    window.requestAnimationFrame(draw);
+}
