bec994a1ac3aad95cb319f9183adeca917cabb17
Thakshak <thakshak120@gmail.com>
2023-03-22 11:33:22 +0800

    Added a new sprite, the animation method is different, But the sacling is a bit off need to fix that

--- scripts/player.js
+++ scripts/player.js
@@ -1,30 +1,49 @@
-class Player{
-    constructor(){
-        this.sprite = {
-            run: new Image(),
-        }
-
-        this.sprite.run.src = "assets/_Run.png";
-
-        this.X = 0;
-        this.Y = 230;
-
-        this.storedTicks = 0;
-        this.animFrame = 0;
+class Player {
+    constructor() {
+      this.sprite = {
+        run: [],
+      };
+  
+      for (let i = 1; i <= 10; i++) {
+        let frame = new Image();
+        frame.src = "assets/run/run_" + i + ".png";
+        this.sprite.run.push(frame);
+      }
+  
+      this.X = 0;
+      this.Y = 230;
+  
+      this.storedTicks = 0;
+      this.animFrame = 0;
+  
+      // Set the scale factor to 1.5 (i.e., 150%)
+      this.scaleFactor = 1.5;
     }
-
-    draw(frame){
-        this.storedTicks += frame;
-
-        this.X += 0.1*frame;
-
-        if(this.storedTicks > 70){
-            this.storedTicks -= 70;
-            this.animFrame = (this.animFrame+1)%10;
-        }
-        ctx.drawImage(this.sprite.run, 40+this.animFrame*120, 42, 29, 38, this.X, this.Y, 29, 38);
+  
+    draw(frame) {
+      this.storedTicks += frame;
+  
+      this.X += 0.1 * frame;
+  
+      if (this.storedTicks > 70) {
+        this.storedTicks -= 70;
+        this.animFrame = (this.animFrame + 1) % 10;
+      }
+  
+      // Scale the player sprite image by the scale factor
+      let width = 27 * this.scaleFactor;
+      let height = 35 * this.scaleFactor;
+  
+      ctx.drawImage(
+        this.sprite.run[this.animFrame],
+        this.X,
+        this.Y,
+        width,
+        height
+      );
     }
-}
+  }
+  
 
 
 
